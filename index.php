1d2f301b9c0fda98ac8ff8dda94e79b717b32c38
aliffebrian15 <68307451+aliffebrian15@users.noreply.github.com>
2020-07-15 09:00:27 +0700

    Update index.php

--- index.php
+++ index.php
@@ -9,4 +9,4 @@
         <body bgcolor=black><table width=100% height=100%><td align=center><span style='font: 60px fantasy;size:60px; color:#c40505; text-shadow: 0px 0px 60px #000000;'>
         Hacked by alf404   </b></center>
         <center><font color=#c6c5c4 size=3 face=courier new>Hexsploit Team</font></center>
-<center><table width=90% height=50%><td align=center><span style='font: 36px courier new; color:#a3a2a1;'><b>hello Kamu</b></center>
+<center><table width=90% height=50%><td align=center><span style='font: 36px courier new; color:#a3a2a1;'><b>BTW LO KONTOL</b></center>
